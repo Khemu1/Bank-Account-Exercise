585bc6430e799b1988cda87a61aa685240855479
khemu_1 <alghost2004@gmail.com>
2023-10-24 01:34:27 +0300

    Updated

--- src/SteppingStone/Main.java
+++ src/SteppingStone/Main.java
@@ -5,25 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         BankAccount obj = new BankAccount();
-        System.out.println("Hello please Enter you Name, Number and you Balance");
+        System.out.println("Hello, please enter your Name, Number, and Balance");
         obj.setName(in.next());
         obj.setNumber(in.next());
         obj.setBalance(in.nextDouble());
-        boolean exit =false;
-        while (!exit)    {
-            System.out.println("To display your info Enter 1");
-            System.out.println("To deposit Money Enter 2 ");
-            System.out.println("To withdraw Money Enter 3 ");
-            System.out.println("To exit enter 4");
-            String input =in.next();
-            switch (String.valueOf(input)) {
+
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("To display your info, enter 1");
+            System.out.println("To deposit Money, enter 2");
+            System.out.println("To withdraw Money, enter 3");
+            System.out.println("To exit, enter 4");
+            String input = in.next();
+
+            switch (input) {
                 case "1" -> obj.printInfo();
-                case "2" -> obj.deposit(in.nextDouble());
-                case "3" -> obj.withdraw(in.nextDouble());
-                case "4" -> exit = true;
+                case "2" -> {
+                    System.out.println("Enter the amount to deposit: ");
+                    obj.deposit(in.nextDouble());
+                }
+                case "3" -> {
+                    System.out.println("Enter the amount to withdraw: ");
+                    obj.withdraw(in.nextDouble());
+                }
+                case "4" -> {
+                    System.out.println("Exiting");
+                    exit = true;
+                }
                 default -> System.out.println("Enter a number between 1-4");
             }
         }
-
     }
 }
