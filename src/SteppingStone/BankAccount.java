ef3cf6ca959d32951c2722ceb4385ef1eccfc45a
khemu_1 <alghost2004@gmail.com>
2023-10-24 00:55:11 +0300

    Updated

--- src/SteppingStone/BankAccount.java
+++ src/SteppingStone/BankAccount.java
@@ -4,58 +4,71 @@ public class BankAccount {
     private String number;
     private String name;
     private double balance;
-    BankAccount()   {
+
+    // Default constructor
+    BankAccount() {
 
     }
-    BankAccount(String number, String name, double balance)   {
-        this.name=name;
-        this.number=number;
-        this.balance=balance;
+
+    // Parameterized constructor
+    BankAccount(String number, String name, double balance) {
+        this.name = name;
+        this.number = number;
+        this.balance = balance;
     }
-    //
+
+    // Getters
     public String getNumber() {
         return number;
     }
-    public  String getName()    {
-        return number;
-    }
-    public  double getBalance() {
-        return  balance;
+
+    public String getName() {
+        return name;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setters
     public void setNumber(String number) {
         this.number = number;
     }
-    public void setName(String name)    {
-        this.name=name;
-    }
-    public void setBalance(double balance)    {
-        this.balance=balance;
-    }
-    //
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Deposit money
     void deposit(double money) {
-        if (money> 0)   {
-            this.balance+=money;
-            System.err.println("your bank account now contains "+this.balance);
-        }
-        else {
-            System.out.println("nothing have been deposited");
+        System.out.println("Enter the amount that should be deposited");
+        if (money > 0) {
+            this.balance += money;
+            System.err.println(money >1 ? money + " $ have been deposited to your account. Your total balance now is " + this.balance + " $":money + " $ has been deposited to your account. Your total balance now is " + this.balance + " $");
+        } else {
+            System.err.println("Nothing has been deposited. Invalid deposit amount.");
         }
     }
-    void withdraw(double money) {
-        if (this.balance >=money)   {
-            this.balance-=money;
-            System.err.println("your bank account now contains "+this.balance);
-        }
-        else    {
-            System.err.println("insufficient funds");
-        }
 
+    // Withdraw money
+    void withdraw(double money) {
+        System.out.println("Enter the amount that should be withdrawn");
+        if (this.balance >= money) {
+            this.balance -= money;
+            System.err.println(money >1 ? money + " $ have been withdrawn from your account. Your total balance now is " + this.balance + " $" :money + " $ has been withdrawn from your account. Your total balance now is " + this.balance + " $");
+        } else {
+            System.err.println("Insufficient funds. Withdrawal failed.");
+        }
     }
-    void printInfo()    {
-        System.err.println(this.name);
-        System.err.println(this.number);
-        System.err.println(this.balance);
+
+    // Print account information
+    void printInfo() {
+        System.err.println("Account Holder: " + this.name);
+        System.err.println("Account Number: " + this.number);
+        System.err.println("Account Balance: " + this.balance + " $");
     }
 }
--- src/SteppingStone/Main.java
+++ src/SteppingStone/Main.java
@@ -5,13 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         BankAccount obj = new BankAccount();
-        System.out.println("Enter your number");
-        obj.setNumber(in.next());
-        System.out.println("Enter your name");
+        System.out.println("Hello pleas Enter you Name, Number and you Balance");
         obj.setName(in.next());
-        System.out.println("Enter your account balance");
+        obj.setNumber(in.next());
         obj.setBalance(in.nextDouble());
+        boolean exit =false;
+        while (!exit)    {
+            System.out.println("To display your info Enter 1");
+            System.out.println("To deposit Money Enter 2 ");
+            System.out.println("To withdraw Money Enter 3 ");
+            System.out.println("To exit enter 4");
+            String input =in.next();
+            switch (String.valueOf(input)) {
+                case "1" -> obj.printInfo();
+                case "2" -> obj.deposit(in.nextDouble());
+                case "3" -> obj.withdraw(in.nextDouble());
+                case "4" -> exit = true;
+                default -> System.out.println("Enter a number between 1-4");
+            }
+        }
 
-        obj.printInfo();
     }
 }
